3cfbfa645de62b255105691d3022535b986670a4
Afonso Gomes Peres <afonsogomesperes@gmail.com>
2026-03-31 22:48:17 +0100

    Added debug win... ts is not tuff

--- client/src/main1.c
+++ client/src/main1.c
@@ -1,14 +1,20 @@
 #ifdef _WIN32
-    #include <curses.h>
-    #include <windows.h>
+#include <curses.h>
+#include <windows.h>
 #else
-    #include <ncurses.h>
-    #include <unistd.h>
+#include <ncurses.h>
+#include <unistd.h>
 #endif
 
 #include <time.h>
 
 #include "./send-file-socket.c"
+
+// GLOBALS
+
+int fps = 0;
+
+// HELPERS
 
 static double now_seconds(void) {
     struct timespec ts;
@@ -16,6 +22,26 @@ static double now_seconds(void) {
     return (double)ts.tv_sec + (double)ts.tv_nsec / 1000000000.0;
 }
 
+void debugWindow(WINDOW *hostw) {
+    int maxy, maxx;
+    getmaxyx(hostw, maxy, maxx);
+
+    WINDOW *debugwin = newwin(6, maxx, 0, 0);
+    if (!debugwin) {
+        endwin();
+        fprintf(stderr, "Failed to create debug window.\n");
+        return;
+    }
+
+    box(debugwin, 0, 0);
+    mvwprintw(debugwin, 1, 1, "Host size: %dx%d", maxx, maxy);
+    mvwprintw(debugwin, 2, 1, "fps: %d", fps);
+    wrefresh(debugwin);
+
+    delwin(debugwin);
+}
+
+// MAIN
 
 int main(void)
 {
@@ -24,7 +50,7 @@ int main(void)
     noecho();
     nodelay(stdscr, TRUE);
     keypad(stdscr, TRUE);
-    curs_set(0);
+    // curs_set(0);
     refresh();
 
 
@@ -34,7 +60,6 @@ int main(void)
     double last_frame_time = now_seconds();
     double last_update_time = last_frame_time;
     int frame_count = 0;
-    int fps = 0;
     double elapsed_seconds = 0.0;
 
 
@@ -60,13 +85,13 @@ int main(void)
             frame_count = 0;
             last_update_time = frame_start;
         }
-        mvprintw(0, 0, "Press 'Q' to quit.\nfps: %d", fps);
-        refresh();
+        debugWindow(stdscr);
 
         double frame_end = now_seconds();
         delta = frame_end - last_frame_time;
         last_frame_time = frame_end;
 
+        // Cap frame rate to ~60 FPS
         if (delta < 1.0 / 60.0) {
             #ifdef _WIN32
                 Sleep((unsigned int)((1.0 / 60.0 - delta) * 1000));
